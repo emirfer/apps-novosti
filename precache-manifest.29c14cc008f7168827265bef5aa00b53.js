a80b6ff6d6a15246a56fd010f4e249a8658ef52f
emirfer <42104197+emirfer@users.noreply.github.com>
2019-07-17 04:11:22 +0200

    Updates

---
+++ precache-manifest.29c14cc008f7168827265bef5aa00b53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b5a47ea0a6e41dfc9537827ecc3600d",
+    "url": "/apps-novosti/index.html"
+  },
+  {
+    "revision": "081ca7696fcf4b62d614",
+    "url": "/apps-novosti/static/css/main.42f4f8db.chunk.css"
+  },
+  {
+    "revision": "78715f753cc530dc6081",
+    "url": "/apps-novosti/static/js/2.0ba5b2f3.chunk.js"
+  },
+  {
+    "revision": "081ca7696fcf4b62d614",
+    "url": "/apps-novosti/static/js/main.ba8aa515.chunk.js"
+  },
+  {
+    "revision": "61418fe060e3405c8540",
+    "url": "/apps-novosti/static/js/runtime~main.14c6f43f.js"
+  }
+]);
